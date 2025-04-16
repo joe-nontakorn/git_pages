92568e639a8d99bd967a08dbfce70a9ce1b14f31
Nontakorn Khnaphol <125560220+joe-nontakorn@users.noreply.github.com>
2025-04-16 20:44:21 +0700

    update blob

--- git_pages/src/components/BackgroundBalls.jsx
+++ git_pages/src/components/BackgroundBalls.jsx
@@ -1,16 +1,14 @@
 // src/components/BackgroundBalls.jsx
 function BackgroundBalls() {
-    return (
-      <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
-        <div className="ball float-ball-1" />
-        <div className="ball float-ball-2" />
-        <div className="ball float-ball-3" />
-        <div className="ball float-ball-4" />
-        <div className="ball float-ball-5" />
+  return (
+    <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
+      <div className="ball" style={{ top: "20%", left: "10%", width: "180px", height: "180px" }}></div>
+      <div className="ball" style={{ top: "60%", left: "25%", width: "200px", height: "200px" }}></div>
+      <div className="ball" style={{ top: "40%", left: "70%", width: "150px", height: "150px" }}></div>
 
-      </div>
-    );
-  }
-  
-  export default BackgroundBalls;
-  
+
+    </div>
+  );
+}
+
+export default BackgroundBalls;
